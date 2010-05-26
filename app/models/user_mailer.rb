faa3e6bf4d2718c4af42a46e822432fcac3a9013
Jon Smith <jonathan@impacti.org>
2010-05-26 23:03:47 +0100

    Now ccs to Jessica, and all should be working. Time for sleep.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ActionMailer::Base
 
   def submission_confirmation(reply)
     recipients  "jonathan@impacti.org"
-    from        "admin@impacti.org"
+    cc          "jlkjones@gmail.com"
+    from        "Wedding RSVP <admin@impacti.org>"
     subject     "RSVP sent from " + reply.name
     body        :reply => reply
   end
